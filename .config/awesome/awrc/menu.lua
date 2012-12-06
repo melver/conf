d4e0edbd770b8396fc12da0f5cb6efd9dd48c00b
Marco Elver <me@marcoelver.com>
2012-12-06 17:14:39 +0000

    awesome/dmenu menus: Use system-state wrapper to suspend/hibernate

--- .config/awesome/awrc/menu.lua
+++ .config/awesome/awrc/menu.lua
@@ -20,7 +20,8 @@ awesomemenu = {
     { "quit",           awesome.quit },
     { "poweroff",       "shutdown-logout poweroff" },
     { "reboot",         "shutdown-logout reboot" },
-    { "suspend",        "xlock -mode blank -startCmd 'sudo pm-suspend'"}
+    { "suspend",        "xlock -mode blank -startCmd 'system-state suspend'"},
+    { "hibernate",      "xlock -mode blank -startCmd 'system-state hibernate'"}
 }
 
 mainmenu = awful.menu({
