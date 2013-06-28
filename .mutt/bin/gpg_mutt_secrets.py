289f91cc3d92f2aebf98671030971cbe8cd2dd24
Marco Elver <me@marcoelver.com>
2013-06-28 20:29:45 +0100

    mutt: gpg_mutt_secrets: Accept quoted passwords

--- .mutt/bin/gpg_mutt_secrets.py
+++ .mutt/bin/gpg_mutt_secrets.py
@@ -4,6 +4,8 @@
 # @file gpg_mutt_secrets.py
 # Can be used as a 'pythonfile' for offlineimap.
 #
+# Tested with Python 2 and 3.
+#
 # @author Marco Elver <me AT marcoelver.com>
 # @date Sun  9 Dec 23:01:30 GMT 2012
 
@@ -21,7 +23,11 @@ def get_mutt_secrets_var(var_name):
     secrets = secrets.split()
     for i,token in enumerate(secrets):
         if token == var_name:
-            return secrets[i+2]
+            pw = secrets[i+2]
+            if pw.startswith(b'"'):
+                return eval(pw).encode()
+            else:
+                return pw
 
     return b''
 
