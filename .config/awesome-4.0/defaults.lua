63fc46c0f07ba8379339aef442cd3b299d8114e9
Marco Elver <me@marcoelver.com>
2017-01-29 15:08:03 +0100

    awesome: Add awesome 4.0 config

---
+++ .config/awesome-4.0/defaults.lua
@@ -0,0 +1,38 @@
+-- Variable definitions
+
+-- Programs
+terminal     = "urxvt"
+terminal_alt = "urxvt -e zsh"
+editor = os.getenv("EDITOR") or "vim"
+editor_cmd = terminal .. " -e " .. editor
+browser = "chromium --disk-cache-size=268435456"
+browser_private = "chromium --incognito --disk-cache-size=268435456"
+mail_client = "term-mail " .. terminal
+modkey = "Mod4"
+
+-- Monitoring
+wifi_netdev = "wlan0"
+sys_battery = "BAT0"
+bat_critical = 10
+show_volume = true
+refresh_delay = 3
+
+top_bar_height = 14
+bottom_bar_height = 13
+
+-- Table of layouts to cover with awful.layout.inc, order matters.
+layouts = {
+    awful.layout.suit.max,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.magnifier,
+    awful.layout.suit.floating,
+    awful.layout.suit.max.fullscreen,
+}
+
+tags = { "1:term", "2:web", "3:work", "4:doc", "5:chat", "6:gfx", "7:media", "8", "9" }
+
