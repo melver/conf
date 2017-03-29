f544d603a0c9611b6fc1c55e696c8af19c614a7d
Marco Elver <me@marcoelver.com>
2017-03-29 23:14:39 +0200

    vim: Bundles update

--- .vim/plugin/bundle_vars.vim
+++ .vim/plugin/bundle_vars.vim
@@ -2,7 +2,7 @@
 let g:necoghc_enable_detailed_browse = 1
 
 " YouCompleteMe
-let g:ycm_filetype_whitelist = { 'cpp':1, 'c':1, 'python':1, 'rust':1, 'ocaml':1 }
+let g:ycm_filetype_whitelist = { 'cpp':1, 'c':1, 'python':1, 'rust':1, 'haskell':1, 'ocaml':1 }
 map <Leader>ycm <ESC>:let g:ycm_filetype_whitelist = {'*':1}<CR>:e<CR>
 let g:ycm_global_ycm_extra_conf = $HOME . "/.vim/bin/ycm_extra_conf.py"
 map <Leader>jd <ESC>:YcmCompleter GoTo<CR>
--- .vim/setup-local-plugins.sh
+++ .vim/setup-local-plugins.sh
@@ -43,14 +43,14 @@ fetch_plugins() {
 
 	# Haskell
 	git clone "https://github.com/dag/vim2hs.git" "vim2hs"
+	git clone "https://github.com/eagletmt/neco-ghc.git" "neco-ghc"
+	echo "au FileType haskell setlocal omnifunc=necoghc#omnifunc" >> "$INIT_BUNDLES_VIM"
 
 	# Dependency for ghcmod-vim
 	#git clone "https://github.com/Shougo/vimproc.vim.git" "vimproc.vim"
 	#(cd "vimproc.vim" && make)
 
-	#git clone "https://github.com/eagletmt/neco-ghc.git" "neco-ghc"
 	#git clone "https://github.com/eagletmt/ghcmod-vim.git" "ghcmod-vim"
-	#echo "au FileType haskell setlocal omnifunc=necoghc#omnifunc" >> "$INIT_BUNDLES_VIM"
 	#echo "au FileType haskell nnoremap <buffer> <Leader>c <ESC>:w<CR>:GhcModCheckAndLintAsync<CR>" >> "$INIT_BUNDLES_VIM"
 	#echo "au FileType haskell nnoremap <buffer> <Leader>x <ESC>:GhcModType<CR>" >> "$INIT_BUNDLES_VIM"
 	#echo "au FileType haskell nnoremap <buffer> <silent> <Leader>z <ESC>:GhcModTypeClear<CR>" >> "$INIT_BUNDLES_VIM"
