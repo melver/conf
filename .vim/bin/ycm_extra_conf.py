af491c131d4df1b9e358e813133987734d647533
Marco Elver <me@marcoelver.com>
2018-08-18 18:54:29 +0200

    vim: Make YCM work with C++ modules

--- .vim/bin/ycm_extra_conf.py
+++ .vim/bin/ycm_extra_conf.py
@@ -3,6 +3,7 @@
 # Behave similar to clang_complete by default.
 
 import os
+import string
 
 DEFAULT_FLAGS = [ '-Wall', '-Wextra', '-Werror' ]
 
@@ -10,10 +11,16 @@ CLANG_COMPLETE = '.clang_complete'
 
 def FileDependentFlags(filename):
     ext = filename.split('.')[-1]
-    if ext in ['cpp', 'cc', 'hpp', 'hh']:
-        return ['-std=c++17', '-x', 'c++']
+    if ext in ['cpp', 'cc', 'hpp', 'hh', 'cxx']:
+        return ['-std=c++17']
     elif ext in ['c']:
-        return ['-std=c99', '-x', 'c']
+        return ['-std=c99']
+    return []
+
+def FixFileType(filename):
+    ext = filename.split('.')[-1]
+    if ext in ['cppm', 'ixx']:
+        return ['-x', 'c++-module']
     return []
 
 def AppendFromClangComplete(filename, flags):
@@ -27,23 +34,40 @@ def AppendFromClangComplete(filename, flags):
 
         if os.path.exists(cc):
             with open(cc, 'r') as f:
-                for line in f:
-                    line = line.strip()
+                for arg in f:
+                    arg = arg.strip()
+                    if not arg: continue
 
                     # Expand relative paths
-                    if line.startswith('-I'):
-                        include_path = line[2:]
+                    if arg.startswith('-I'):
+                        include_path = arg[2:]
                         if not include_path.startswith('/'):
                             include_path = os.path.join(curpath, include_path)
-                            line = '-I' + os.path.normpath(include_path)
+                            arg = '-I' + os.path.normpath(include_path)
+                    elif arg.startswith('-fmodule-file='):
+                        module = None
+                        # Find module name from source file
+                        with open(filename, 'r') as f:
+                            line_num = 0
+                            for line in f:
+                                # only check first 100 lines
+                                line_num += 1
+                                if line_num > 100: break
+                                if line.startswith('module'):
+                                    module = line.split()[-1].strip(";")
+                                    break
 
-                    flags.append(line)
+                        # If not found, skip this arg.
+                        if module is None: continue
+                        arg = string.Template(arg).substitute(module=module)
+
+                    flags.append(arg)
                 return True
 
     return False
 
 def FlagsForFile(filename, **kwargs):
-    flags = []
+    flags = FixFileType(filename)
 
     if not AppendFromClangComplete(filename, flags):
         flags += DEFAULT_FLAGS
